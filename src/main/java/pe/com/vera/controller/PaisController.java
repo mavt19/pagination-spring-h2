dd56cf195c13e90ac2bd760d0b500a513a795b2b
mavt19 <mavt10rm@gmail.com>
2020-09-01 08:22:44 -0500

    agrgando version 14 de java

--- src/main/java/pe/com/vera/controller/PaisController.java
+++ src/main/java/pe/com/vera/controller/PaisController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,7 +22,7 @@ public class PaisController {
 
 	private final PaisService<Pais> _services;
 	
-	@GetMapping("/paises")
+	@PostMapping("/paises")
 	public ResponseEntity<Page<Pais>> paginas(
 			@RequestParam(defaultValue = "0")int page,
 			@RequestParam(defaultValue = "5")int size,
